4329bd6de8859bf44ded6fe391dbad4a63fa4bb9
ramirezmz <roberto.paolo@dremio.com>
2022-04-02 00:22:51 -0300

    add: practice todo-list

---
+++ study-vuex/todo-list-app/todo/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="" id="app">
+    <div class="container grid-xs py-2">
+      <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Logo do Vue">
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" 
+            v-model="todo.description"
+            class="form-input" placeholder="Novo todo">
+            
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <div class="todo-list">
+        <!-- renderizar a lista  :key = é para o vue nao se perder-->
+        <Todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" @remove="removeTodo" :todo="t" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from "./components/Todo.vue"
+export default {
+  name: 'App',
+  components: { Todo },
+  data() {
+    return { 
+      todos: [], 
+      todo: { checked: false } }
+    },
+    methods: {
+      addTodo(todo) {
+        //adicionar um ID
+        todo.id = Date.now()
+        this.todos.push(todo)
+
+        this.todo = { checked: false}
+      },
+      toggleTodo(todo) {
+        const index = this.todos.findIndex(item => item.id === todo.id)
+        if( index > -1) {
+          const checked = !this.todos[index].checked
+          this.todos[index].checked = checked
+        }
+      },
+      removeTodo(todo) {
+        const index = this.todos.findIndex(item => item.id === todo.id)
+        if(index > - 1) {
+          this.todos.splice(index, 1)
+        }
+      }
+  }
+}
+</script>
+<style scoped>
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
+}
+
+.todo-list {
+  padding-top: 2rem;
+}
+</style>
---
+++ study-vuex/todo-list-app/todo/src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tile flex-centered" :class="{checked: todo.checked}">
+      <div class="tile-icon">
+        <i class="icon" :class="todo.checked ? 'icon-check' : 'icon-time'"></i>
+      </div>
+      <div class="tile-content">
+        <div class="tile-subtitle">{{todo.description}}</div>
+      </div>
+        <div class="tile-action">
+      <button @click="$emit('toggle', todo)" class="btn btn-link">
+        <span v-if="todo.checked">Desmarcar</span>
+        <span v-else>Concluído</span>
+        </button>
+      <button class="btn btn-link">
+        <span @click="$emit('remove', todo)" class="text-error">Remover</span>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    todo: { type: Object, require: true}
+  }
+}
+</script>
+<style scoped>
+.checked {
+  text-decoration: line-through;
+  color: lightgray;
+}
+</style>
