0c021b717facc5ecdf137f14c480a51345e7f3ed
ramirezmz <roberto.paolo@dremio.com>
2022-04-23 21:06:29 -0300

    test

--- prof-pablo-codes/primeiros_passos/app.js
+++ prof-pablo-codes/primeiros_passos/app.js
@@ -1,43 +1,44 @@
-const { createApp, ref, reactive, computed, watch} = Vue
-
+const { createApp, ref, reactive, computed, watch } = Vue;
+// testando
 const app = createApp({
-   setup() {
-      const contador = ref(0)
-      const somaContador = ref(0)
-      const data = reactive({
-         titulo: "Robertopaolo asdasd"
-      })
-      const idade = 16
+  setup() {
+    const contador = ref(0);
+    const somaContador = ref(0);
+    const data = reactive({
+      titulo: "Robertopaolo asdasd",
+    });
+    const idade = 16;
 
-      function adicionar() {
-         contador.value ++
-      }
-      function resetarContador() {
-         contador.value = 0
-      }
-      function somarContador() {
-         somaContador.value = contador.value * 10
-      }
+    function adicionar() {
+      contador.value++;
+    }
+    function resetarContador() {
+      contador.value = 0;
+    }
+    function somarContador() {
+      somaContador.value = contador.value * 10;
+    }
 
-      const eMaior = computed(() => idade > 18)
-      const nomeCompleto = computed(() => 'Pedro'+ 'de Oliveira')
+    const eMaior = computed(() => idade > 18);
+    const nomeCompleto = computed(() => "Pedro" + "de Oliveira");
 
-      watch(contador, (novoValor) => {
-         if(novoValor > 10) {
-            contador.value = 0
-         }
-      })
-      return {
-         contador,
-         adicionar,
-         data,
-         resetarContador,
-         somarContador,
-         somaContador,
-         eMaior,
-         nomeCompleto
+    watch(contador, (novoValor) => {
+      if (novoValor > 10) {
+        contador.value = 0;
       }
-   }
-})
+    });
+    return {
+      contador,
+      adicionar,
+      data,
+      resetarContador,
+      somarContador,
+      somaContador,
+      eMaior,
+      nomeCompleto,
+    };
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
+
