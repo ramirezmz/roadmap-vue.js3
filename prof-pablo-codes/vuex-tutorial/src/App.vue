9fb4cf1a53ebadff0bafa071bacb6fa47306cabf
ramirezmz <roberto.paolo@dremio.com>
2022-04-30 14:31:42 -0300

    add: base vuex

---
+++ prof-pablo-codes/vuex-tutorial/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavBar />
+  <router-view />
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+img {
+  max-width: 100%;
+}
+</style>
---
+++ prof-pablo-codes/vuex-tutorial/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <ListaUsuario />
+</template>
+
+<script>
+import ListaUsuario from "@/components/ListaUsuario.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ListaUsuario,
+  },
+};
+</script>
